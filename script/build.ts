268631e4bea90a164378e722af1422dcdf004b5d
Lucas Garron <code@garron.net>
2025-08-08 07:53:04 -0700

    [build.ts] Update build config usage.

--- script/build.ts
+++ script/build.ts
@@ -1,9 +1,9 @@
-import { es2022 } from "@cubing/dev-config/esbuild/es2022";
+import { es2022Lib } from "@cubing/dev-config/esbuild/es2022";
 import { $ } from "bun";
 import { build } from "esbuild";
 
 await build({
-  ...es2022,
+  ...es2022Lib(),
   entryPoints: ["src/index.ts"],
   outdir: "./dist/lib/printable-shell-command/",
   sourcemap: true,
